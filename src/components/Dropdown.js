4ad27f2f98639b0c7baece02f87cdf99fdd401fe
Redmond Chan <redmondchan92@gmail.com>
2019-07-21 12:42:10 -0400

    added faq content and some css for it

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -33,9 +33,18 @@ class DropdownMenu extends React.Component {
             <span className='text'>Log Out</span>
           </Dropdown.Item>
           <Modal trigger={<Dropdown.Item><Icon name='question'/><span className="text">FAQ</span></Dropdown.Item>} closeIcon>
-            <Modal.Header>FAQ</Modal.Header>
+            <Modal.Header className="faq-header">FAQ</Modal.Header>
             <Modal.Content>
-            Testing!
+              <h4>How do I keep the baby's HP above 0?</h4>
+                <p className='faq-p'>Feed the baby about every 3 hours and change their diaper about every 2.5 hours.</p>
+              <h4>What is the heart symbol on the top right?</h4>
+                <p className='faq-p'>It'll tell you how many days you've kept the baby's hp above 0.</p>
+              <h4>Can I "revive" my baby?</h4>
+                <p className='faq-p'>Yes, just feed or change their diaper at the right time.</p>
+              <h4>How are the expenses calculated?</h4>
+                <p className='faq-p'>It's a rough estimate of local prices here. At the moment, one diaper is $0.30 and one feeding is $1.50.</p>
+              <h4>Can I change my baby's name?</h4>
+                <p className='faq-p'>Maybe in the future. Names are currently being randomly generated through an API.</p>
             </Modal.Content>
           </Modal>
         </Dropdown.Menu>
